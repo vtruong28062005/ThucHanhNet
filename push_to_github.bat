8f03116e76b2960988a6fb4ba95ef63b9e3f79fb
vtruong28062005 <vtruong28062005@users.noreply.github.com>
2026-09-24 12:59:11 +0700

    Update push_to_github.bat encoding

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 chcp 65001 > nul
 set "PATH=C:\Users\Truong\AppData\Local\Programs\Git\cmd;C:\Users\Truong\AppData\Local\Programs\Git\mingw64\bin;%PATH%"
 cd /d "D:\ThucHanhNet"
@@ -8,7 +8,7 @@ echo   PUSH CODE LEN GITHUB: vtruong28062005/ThucHanhNet
 echo ========================================================
 echo.
 echo Dang thuc hien: git push -u origin main ...
-echo (Neu co cua so trinh duyet bat len, vui long bam Authorize / Dang nhap)
+echo (Neu co hop thoai GitHub dang nhap, ban hay chon 'Sign in with your browser')
 echo.
 git push -u origin main
 echo.
